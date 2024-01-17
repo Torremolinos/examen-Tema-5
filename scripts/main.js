16ae202b3e2438ff2a6587dab09a6f841af8a16a
Torremolinos <adrian_2054@hotmail.com>
2024-01-17 09:28:19 +0100

    variables de css y variables de js

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,14 @@
  * @Author : Adrian Iglesias Riño
  * @Github : 
  */
+
+
+let nombre = document.querySelector("#nombre");
+let email = document.querySelector("#email");
+let informacion = document.querySelector("#informacion");
+let reserva = document.querySelector("#reserva");
+let info = document.querySelector("#info");
+let mensaje = document.querySelector("#mensaje");
+let enviar = document.querySelector("#enviar");
+
+erorres = [];
